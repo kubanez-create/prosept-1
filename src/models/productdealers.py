38a1e31714229256102b427950960b97e3314028
kubanez-create <zo22@mail.ru>
2023-12-06 00:12:21 +0400

    all seems to work but predictions are not correct

--- src/models/productdealers.py
+++ src/models/productdealers.py
@@ -11,24 +11,24 @@ class ProductDealer(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     key: Mapped[str]
-    date: Mapped[date]
+    # date: Mapped[date]
     dealer_id: Mapped[int] = mapped_column(
         ForeignKey("dealer.id"), primary_key=True
     )
     product_id: Mapped[int] = mapped_column(
         ForeignKey("product.id"), primary_key=True
     )
-    keys: Mapped["DealerPrice"] = relationship(
-        primaryjoin="ProductDealer.key==DealerPrice.product_key",
-        # primaryjoin="and_(DealerPrice.dealer_id == ProductDealer.dealer_id, "
-        # "ProductDealer.key==DealerPrice.product_key)"
-    )
+    # keys: Mapped["DealerPrice"] = relationship(
+    #     # primaryjoin="ProductDealer.key==DealerPrice.product_key",
+    #     primaryjoin="and_(DealerPrice.dealer_id == ProductDealer.dealer_id, "
+    #     "ProductDealer.key==DealerPrice.product_key)"
+    # )
 
     dealers: Mapped["Dealer"] = relationship(back_populates="productdealers")
     products: Mapped["Product"] = relationship(back_populates="proddealers")
-    __table_args__ = (
-        ForeignKeyConstraint(
-            ['key', "date", 'dealer_id'],
-            ['dealerprice.product_key', 'dealerprice.date', 'dealerprice.dealer_id']
-        ),
-    )
+    # __table_args__ = (
+    #     ForeignKeyConstraint(
+    #         ['key', 'dealer_id'],
+    #         ['dealerprice.product_key', 'dealerprice.dealer_id']
+    #     ),
+    # )
