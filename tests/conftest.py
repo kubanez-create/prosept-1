98bac8fe94c833e81f280d9de9ab82e151d0b046
kubanez-create <zo22@mail.ru>
2023-12-06 12:14:03 +0400

    changed tests setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
-import asyncio
 from pathlib import Path
+import asyncio
+import sys
 from typing import AsyncGenerator
 
 import pytest
@@ -9,31 +10,31 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
-from db.db import get_async_session
-from src import metadata
-from src.main import app
+sys.path.append("")
 
+from src.db.db import get_async_session
+from src.main import app
+from src.models import Base
+
+print("START")
 # DATABASE
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-TEST_DB = BASE_DIR / "test.db"
-DATABASE_URL_TEST = f"sqlite+aiosqlite:///{str(TEST_DB)}"
+TEST_DB = BASE_DIR / 'test.db'
+DATABASE_URL_TEST = f'sqlite+aiosqlite:///{str(TEST_DB)}'
 
+metadata = Base.metadata
 engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
-async_session_maker = sessionmaker(
-    engine_test, class_=AsyncSession, expire_on_commit=False
-)
+async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
 metadata.bind = engine_test
-
-
+print("METADATA", metadata)
 async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
 
-
 app.dependency_overrides[get_async_session] = override_get_async_session
-
-
-@pytest.fixture(autouse=True, scope="session")
+print(app.dependency_overrides)
+print("DEPENDENCY")
+@pytest.fixture(autouse=True, scope='session')
 async def prepare_database():
     async with engine_test.begin() as conn:
         await conn.run_sync(metadata.create_all)
@@ -41,20 +42,17 @@ async def prepare_database():
     async with engine_test.begin() as conn:
         await conn.run_sync(metadata.drop_all)
 
-
 # SETUP
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
-
 client = TestClient(app)
-
 
 @pytest.fixture(scope="session")
 async def ac() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(app=app, base_url="http://test", follow_redirects=True) as ac:
         yield ac
---
+++ tests/test_product.py
@@ -0,0 +1,32 @@
+from httpx import AsyncClient
+
+
+async def test_add_specific_operations(ac: AsyncClient):
+    response = await ac.post("/api/products/add", json={
+        "id": 2,
+        "article": "257-34",
+        "ean_13": 17462873,
+        "name": "Something something",
+        "cost": 230,
+        "recommended_price": "540",
+        "category_id": 20,
+        "ozon_name": "string",
+        "name_1c": "string",
+        "wb_name": "string",
+        "ozon_article": 76,
+        "wb_article": 43,
+        "ym_article": "string",
+        "wb_article_td": "string"
+    })
+
+    assert response.status_code == 201
+    assert response.json()["id"] == 2
+
+# # @pytest.mark.asyncio
+# async def test_get_specific_operations(ac: AsyncClient):
+#     response = await ac.get("/api/products")
+
+#     assert response.status_code == 200
+#     print(response.json())
+#     assert response.json()["status"] == "success"
+#     assert len(response.json()["data"]) == 1
